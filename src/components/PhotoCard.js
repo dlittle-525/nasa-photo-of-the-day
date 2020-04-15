833269ca25fc2e6956e1c44a56ee2dd3e4a318f2
Douglas Little <douglas.a.little@gmail.com>
2020-04-14 23:16:54 -0400

    MVP+

---
+++ src/components/PhotoCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function PhotoCard(props) {
+    return (
+        <div>
+            <h2>{props.title}</h2>
+            <img src={props.url} alt = '' />
+            <br />
+            <p>{props.explanation}</p>
+        </div>
+    )
+}
+
+export default PhotoCard;
